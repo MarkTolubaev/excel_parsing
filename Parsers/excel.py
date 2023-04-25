c7ca6c0cee5cc7506182c122b7e064908596a5e2
Mark <tolubaev@it.domeo.ru>
2023-04-25 10:24:31 +0300

    new parsing algorithm

--- Parsers/excel.py
+++ Parsers/excel.py
@@ -23,26 +23,18 @@ class ParserError(Exception):
 
 class Parser:
     def __init__(self, file: str):
-        """
-        Инициализация объекта
-
-        :param file: путь к файлу для парсинга
-        """
         try:
             self._dataFrame = read_excel(file, header=[0, 1, 2])
         except Exception as e:
             raise ParserError(f'Ошибка чтения файла: {e.__class__.__name__}: {str(e)}')
 
     def do_parsing(self):
-        data1 = []
-        for _, v in self._dataFrame.loc[:, (['id', 'company'],)].droplevel([1, 2], axis=1).iterrows():
-            data1.append(v.to_dict())
-
-        data2 = []
-        for k, v in self._dataFrame.loc[:, (['fact', 'forecast'],)].stack([0, 1]).iterrows():
-            data2.append(dict(zip(('analytics', 'q_type'), k[1:])) | v.to_dict())
-
-        res = [item[0] | item[1] | item[2] for item in zip(data1, data2, self.__moc_data(len(data1)))]
+        df2 = self._dataFrame.loc[:, (['fact', 'forecast'],)].stack([0, 1]).reset_index(names=['index', 'analytics', 'q_type'])
+        df1 = self._dataFrame.loc[:, (['id', 'company'],)].droplevel([1, 2], axis=1).reset_index(names='index')
+        normalized_table = df2.join(df1, on='index', rsuffix='_right', lsuffix='_left')
+        normalized_table = normalized_table.loc[:, ~normalized_table.columns.isin(['index_right', 'index_left'])]
+        [r[1].to_dict() for r in normalized_table.iterrows()]
+        res = [r[1].to_dict() | {"date": self.__moc_date()} for r in normalized_table.iterrows()]
 
         self.__save_to_db(res)
 
@@ -53,5 +45,5 @@ class Parser:
             ses.commit()
 
     @staticmethod
-    def __moc_data(length):
-        return [{"date": getData(datetime(year=2023, month=4, day=randint(1, 31)))} for _ in range(length)]
+    def __moc_date():
+        return getData(datetime(year=2023, month=4, day=randint(1, 30)))
--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from Parsers import ExcelParser
-from DB import init_db
+from DB import init_db, Session
+from DB.models import TestTable
 
 
 def main():
     init_db()
+
+    with Session() as ses:
+        ses.query(TestTable).delete()
+        ses.commit()
+
     file = r'.\temp\test_file.xlsx'
     parser = ExcelParser(file)
     parser.do_parsing()
